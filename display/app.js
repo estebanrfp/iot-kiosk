78c6a8d451fae1c7f2102ee4d21d5ab8599f6af4
estebanrfp <estebanrfp@gmail.com>
2017-05-21 13:42:56 +0200

    update Dockercompose

---
+++ display/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var PORT = 3000;
+var app = express();
+
+app.get('/', function (req, res){
+  res.send('Hello world & Docker!');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
